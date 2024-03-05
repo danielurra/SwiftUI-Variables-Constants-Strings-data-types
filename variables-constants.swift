71a99e1ac0539f79c4f74ba610a950452bfc9cd4
Daniel Urra <danielurra75@gmail.com>
2024-03-05 05:55:39 -0500

    Create variables-constants.swift

---
+++ variables-constants.swift
@@ -0,0 +1,16 @@
+//Variable (its value can change)
+var age = 33
+age = 34
+age = 35
+age = 36
+//Camel-Case Camel-hump notation (lower 1rst word then uppercase)
+var ageOfEusebio = 33
+//Constant (its value doesn't change)
+let kilosToLibs = 2.2
+//String (Text)
+var firstname = "Eusebio"
+var lastname = "Taba"
+//Data Types - Integer (Whole number)
+var ageV2: Int = 33
+//Data Types - Double (decimal point)
+let kilosToLibsV2: Double = 2.2
